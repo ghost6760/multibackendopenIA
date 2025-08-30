cb59d0158611b5793c6dc2513b286ea7d1bfa969
ghost6760 <146762829+ghost6760@users.noreply.github.com>
2025-08-29 23:10:17 -0500

    Create __init__.py

---
+++ app/config/__init__.py
@@ -0,0 +1,9 @@
+from .settings import Config, DevelopmentConfig, ProductionConfig, TestingConfig, config
+
+__all__ = [
+    'Config',
+    'DevelopmentConfig',
+    'ProductionConfig',
+    'TestingConfig',
+    'config'
+]
