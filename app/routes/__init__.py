1055f6ff7db255e21bf6b5ff4005a5fe6c945d2a
ghost6760 <146762829+ghost6760@users.noreply.github.com>
2025-09-01 04:47:56 -0500

    Create __init__.py

---
+++ app/routes/__init__.py
@@ -0,0 +1,7 @@
+# app/routes/__init__.py
+"""
+Routes package initialization
+Makes the routes directory a proper Python package
+"""
+
+# This file makes the routes directory importable as a Python package
