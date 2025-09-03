8c55386c09f33cb706e96761b07eb17226e00cc7
ghost6760 <146762829+ghost6760@users.noreply.github.com>
2025-09-03 11:00:41 -0500

    Create LoadingSpinner.js

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,13 @@
+// src/components/LoadingSpinner.js - Componente de loading
+
+import React from 'react';
+
+const LoadingSpinner = ({ size = 'medium', className = '' }) => {
+  const sizeClasses = {
+    small: 'w-4 h-4',
+    medium: 'w-8 h-8', 
+    large: 'w-12 h-12'
+  };
+
+  return (
+    <div className={`
